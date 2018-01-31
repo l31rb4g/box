0450f49b00781994d5d6cce7828e6514a1b53b61
Gabriel Paladino <l31rb4g@gmail.com>
2018-01-31 11:50:03 -0200

    newbox

--- box/newbox.py
+++ box/newbox.py
@@ -12,8 +12,22 @@ class Box:
         self.path = path
         # _content = self._walk()
         # self._write_boxfile(dump(_content))
-        self.bucket = Bucket()
-        print(self.bucket.ls())
+        # self.bucket = Bucket()
+        # print(self.bucket.ls())
+        self._list()
+
+    def _list(self):
+        _l = []
+        for (dirpath, dirnames, filenames) in os.walk(self.path):
+            for f in filenames:
+                if f != '.box':
+                    _path = dirpath.replace(self.path, '')
+                    if _path:
+                        _path = _path[1:] + '/'
+                        if _path not in _l:
+                            _l.append(_path)
+                    _l.append(_path + f)
+        print(_l)
 
     def _walk(self):
         _list = {}
@@ -31,7 +45,11 @@ class Box:
         return _list
 
     def _write_boxfile(self, content):
+        _head = '# ' + ('=' * 41) + ' #'
+        _head += "\n# Auto-generated file. Please do not edit.  #\n"
+        _head += '# ' + ('=' * 41) + ' #\n\n'
         with open(self.path + '/' + self.boxfile, 'w') as f:
+            content = _head + content
             f.write(content)
 
     def _read_boxfile(self):
