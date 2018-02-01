1e8d232ca703457d5a665f0562e0565e01a75c31
Gabriel Paladino <gabriel.paladino@stormsec.com.br>
2018-02-01 19:27:12 -0200

    kde notification

--- box/kde_notification.py
+++ box/kde_notification.py
@@ -8,10 +8,7 @@ class KdeNotification():
         create_handle = self._get_dbus_interface('org.kde.kuiserver', '/JobViewServer', 'org.kde.JobViewServer')
         request_path = create_handle.requestView('KdeNotification', 'KDE Notification', 0)
         request_handle = self._get_dbus_interface('org.kde.kuiserver', request_path, 'org.kde.JobViewV2')
-        request_handle.setInfoMessage('Echoing files in /etc')
-
-        while True:
-            pass
+        request_handle.setInfoMessage('Message here...')
 
     def _get_dbus_interface(self, name, path, interface):
         obj = self.session.get_object(name, path)
